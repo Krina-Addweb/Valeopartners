e34b6fc1d803cfe6bd01f93c0e8d2da6edb45d07
krina-addweb <krina.addweb@gmail.com>
2020-12-21 15:21:36 +0530

    All files

---
+++ src/test/java/Constants/CommonVar.java
@@ -0,0 +1,9 @@
+package Constants;
+
+public class CommonVar {
+    public static final String uname = "valeodev";
+    public static final String pass = "F0rD3v0nly!";
+    public static final String ht_uname = "valeo_pinaldesai";
+    public static final String ht_pass = "valeo_pinaldesai";
+//    public static final String URL = "https://" + ht_uname + ":" + ht_pass + "@" + "dev.reports.valeopartners.com";
+}
