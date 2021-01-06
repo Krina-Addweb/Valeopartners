2bd8e73c19c94818ff18f74c8f28d75c746ff723
Krina-Addweb <krina.addweb@gmail.com>
2021-01-06 14:01:41 +0530

    Add files via upload

---
+++ src/test/java/com/valeopartnersautmation/TestCase/BaseCase.java
@@ -0,0 +1,40 @@
+package com.valeopartnersautmation.TestCase;
+
+import com.valeopartnersautmation.Constants.CommonVars;
+import com.valeopartnersautmation.Reports.ReportClass;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeTest;
+
+public class BaseCase extends ReportClass {
+
+    public static WebDriver driver;
+
+    @BeforeTest
+    public void initialize() {
+//        System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir") + "chromedriver");
+//        driver = new ChromeDriver();
+//        CommonVars constantVars = new CommonVars();
+//        driver.get(constantVars.url);
+//        driver.manage().window().maximize();
+
+//        headless
+        System.setProperty("Webdriver.chrome.driver", System.getProperty("user.dir") + "chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        // options.setExperimentalOption("useAutomationExtension", false);
+        options.addArguments("--headless");
+        options.addArguments("window-size=1200,600");
+        options.addArguments("--disable-extensions"); // disdemoabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
+//        driver = new ChromeDriver();
+        driver.get("https://valeodev:F0rD3v0nly!@dev.reports.valeopartners.com/");
+//      CommonVar constantVars = new CommonVar();
+//      driver.get("https://valeodev:F0rD3v0nly!@dev.reports.valeopartners.com/");
+        driver.manage().window().maximize();
+        System.out.println("Title of the page is -> " + driver.getTitle());
+    }
+}
---
+++ src/test/java/com/valeopartnersautmation/TestCase/TestValeoReport.java
@@ -0,0 +1,28 @@
+package com.valeopartnersautmation.TestCase;
+
+import com.valeopartnersautmation.Pages.ValeoReport;
+import org.testng.annotations.Test;
+import java.io.IOException;
+
+public class TestValeoReport extends BaseCase{
+    @Test(priority = 1)
+    public void Adminlogin() throws IOException, InterruptedException {
+        logger = extent.createTest("Check if admin is logged in");
+        ValeoReport valeoReport = new ValeoReport(driver, logger);
+        valeoReport.login();
+    }
+
+    @Test(priority = 2)
+    public void graphCheck() throws IOException, InterruptedException {
+        logger = extent.createTest("Check if the graph is present on the Graph page");
+        ValeoReport valeoReport = new ValeoReport(driver, logger);
+        valeoReport.checkGraph();
+    }
+
+    @Test(priority = 3)
+    public void reportData() throws IOException, InterruptedException {
+        logger = extent.createTest("Check if the graph values and report values are same or not");
+        ValeoReport valeoReport = new ValeoReport(driver, logger);
+        valeoReport.getReportData("Kirkland & Ellis LLP", "2020", "High", "Senior Partner");
+    }
+}
